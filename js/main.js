853a2d091f217cce2695930afecc0db15ab08ef0
Daniel Leite <daniel.leite@comcarde.com>
2022-10-31 15:44:34 +0000

    add animations to the sections using Intersection Observer API

--- js/main.js
+++ js/main.js
@@ -29,3 +29,56 @@ window.onscroll = function () {
   let scrolled = (window.pageYOffset / sectionJumplink.offsetTop) * 100;
   document.getElementById('progressBar').style.width = scrolled + '%';
 }
+
+// Intersections
+const allSections = document.querySelectorAll('section');
+const cardSection = document.querySelector('.section-card');
+const selectedSection = document.querySelector('.selected-section');
+const sectionJumplink = document.querySelector('.section-after-jumplink');
+const specialSections = ['section-card', 'section-after-jumplink', 'section-after-jumplink-two']
+
+const aside = document.querySelector('aside');
+
+const config = {
+  rootMargin: `${document.querySelector('aside').offsetHeight}px`,
+  threshold: 1
+};
+
+const jumpLinkSectionsObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if(!entry.isIntersecting) return 
+    
+    if(!specialSections.includes(entry.target.className)) {
+      selectedSection.innerHTML = entry.target.innerText
+      aside.classList.remove('bottom-aside');
+      aside.classList.add('top-aside');
+      showButton.classList.remove('hidden');
+      showButton.classList.add('block');     
+    } else if(entry.target.className === 'section-after-jumplink') {
+      nav.classList.remove('open');
+      nav.classList.add('close');
+      showButton.innerHTML = 'Show';
+    }
+  });
+}, config);
+
+const cardSectionObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if(!entry.isIntersecting) return
+     
+      aside.classList.add('bottom-aside');
+      aside.classList.remove('top-aside');
+      showButton.classList.remove('block');
+      showButton.classList.add('hidden');
+      nav.classList.remove('open');
+      nav.classList.add('close');
+      selectedSection.innerHTML = 'Card Component';
+      showButton.innerHTML = 'Show';
+  })
+}, {threshold: 0});
+
+allSections.forEach((section) => {
+  jumpLinkSectionsObserver.observe(section);
+});
+
+cardSectionObserver.observe(cardSection)
