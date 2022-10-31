d0a72fe2d204504dbd2d445440f653461dcc2654
Daniel Leite <daniel.leite@comcarde.com>
2022-10-31 12:26:51 +0000

    add progress bar

--- js/main.js
+++ js/main.js
@@ -22,3 +22,10 @@ navLinks.forEach((anchor) => {
     showButton.innerHTML = 'Show';
   });
 })
+
+
+// Smooth Progress Bar
+window.onscroll = function () {
+  let scrolled = (window.pageYOffset / sectionJumplink.offsetTop) * 100;
+  document.getElementById('progressBar').style.width = scrolled + '%';
+}
